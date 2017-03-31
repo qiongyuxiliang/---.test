635b8afafbed0b579b13186c35c9667c20857d1f
756620347@qq.com <756620347@qq.com>
2017-03-31 11:18:54 +0800

    二维码实现
    前端之路

--- app_download/js/index.js
+++ app_download/js/index.js
@@ -1,41 +1,40 @@
 $(document).ready(function(){
+		function code () {	
 	var url=window.location.href;
 	var qrcode = new QRCode($('.qrcode')[0], {
 	width : 100,
 	height : 100
 });
-
-function makeCode () {		
 	
 	if (!url) {
 		return;
 	}
-	qrcode.makeCode(url);
-}
-
-$('.create_rqcode').on({
-	click:function(){
-		$('.rqcode')[0].makecode().show();
-
-	},
-	touchend:function(){
-		$('.rqcode')[0].makecode().show();
-
+	else
+	{
+		qrcode.makeCode(url);
 	}
-
-})
-if($('.create_rqcode').show()){
-	$('body').on({
-	click:function(){
-		$('.rqcode').hide();
-
-	},
-	touchend:function(){
-		$('rqcode').hide();
-
-	}
-})
+	
 }
-
-
+ // var q=document.getElementsByClassName('qrcode');
+ $('.create_qrcode').one('click', function(e){
+        code();
+        // 把生成二维码的函数只绑定一次
+        e.preventDefault(); 
+        
+		// $('.x').show();
+		// return  $('.create_qrcode');
+ }).on('click', function(event) {
+ 	event.preventDefault();
+ 	$('.qrcode').show();
+	$('.q-code').show();
+ 	/* Act on the event */
+ });
+ $('.q-code').on('click',function(){
+ 	$('.qrcode').hide();
+	$('.q-code').hide();
+ })
+ 
 })
+
+
+
